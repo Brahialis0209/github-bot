abe9a73fb26e28430db2001a99d06d9fa9b74f30
Brahialis0209 <rafantem@mail.ru>
2022-04-16 22:56:58 +0300

    down text decorator lower

--- main.py
+++ main.py
@@ -53,12 +53,6 @@ def start_message(message):
 
 
 
-@bot.message_handler(content_types=['text'])
-def send_text(message):
-    if message.text == Answers.user_inf:
-        bot.send_message(message.chat.id, User.ans, reply_markup=user_opts.start_kb_for_user())
-
-
 
 def is_user_add(data):
     return User.user_add in data.split(' ')
@@ -80,6 +74,12 @@ def user_adding(message):
     # dbworker.set_state(message.chat.id, config.States.S_ENTER_AGE.value)
 
 
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    print("GGG4")
+    if message.text == Answers.user_inf:
+        bot.send_message(message.chat.id, User.ans, reply_markup=user_opts.start_kb_for_user())
 
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
