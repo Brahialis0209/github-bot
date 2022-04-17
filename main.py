38d224f49e117023fb4eec806f6b393113ed2076
Brahialis0209 <rafantem@mail.ru>
2022-04-17 16:45:46 +0300

    debuf 1 step

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ def user_adding(message):
 def alias_adding(message):
     user_id = message.from_user.id
     alias = message.text
-    db_object.execute(f"SELECT gh_username FROM gh_users WHERE tg_alias_user = {alias}")
+    db_object.execute("SELECT gh_username FROM gh_users WHERE tg_alias_user = %s", (alias))
     result = db_object.fetchone()
     print("GGG6")
     if not result:
